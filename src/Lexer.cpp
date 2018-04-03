e9d488048edb7bb14200a5d085a45ce0e0acf736
Felix Klauke <info@felix-klauke.de>
2018-04-03 09:28:54 +0200

    Improve character peeking and take program input instead of hardcoded assumptions.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -14,6 +14,7 @@ Token Lexer::readNextToken() {
     if (endOfFileIsReached) {
         return Token("END_OF_FILE", TokenType::END_OF_FILE);
     }
+
     pollCharacter();
 
     if (iswspace(currentCharacter)) {
@@ -21,20 +22,20 @@ Token Lexer::readNextToken() {
     }
 
     if (currentCharacter == '{') {
-        return Token("{", TokenType::LEFT_CURLY_BRACE);
+        return Token(std::string(1, currentCharacter), TokenType::LEFT_CURLY_BRACE);
     } else if (currentCharacter == '}') {
-        return Token("}", TokenType::RIGHT_CURLY_BRACE);
+        return Token(std::string(1, currentCharacter), TokenType::RIGHT_CURLY_BRACE);
     } else if (currentCharacter == '(') {
-        return Token("(", TokenType::LEFT_BRACE);
+        return Token(std::string(1, currentCharacter), TokenType::LEFT_BRACE);
     } else if (currentCharacter == ')') {
-        return Token(")", TokenType::RIGHT_BRACE);
+        return Token(std::string(1, currentCharacter), TokenType::RIGHT_BRACE);
     } else if (currentCharacter == '+') {
-        return Token("+", TokenType::PLUS);
+        return Token(std::string(1, currentCharacter), TokenType::PLUS);
     } else if (currentCharacter == '-') {
-        return Token("-", TokenType::MINUS);
+        return Token(std::string(1, currentCharacter), TokenType::MINUS);
     }
 
-    return Token("UNKNOWN", TokenType::UNKNOWN);
+    return Token(std::string(1, currentCharacter), TokenType::UNKNOWN);
 }
 
 void Lexer::pollCharacter() {
@@ -43,11 +44,11 @@ void Lexer::pollCharacter() {
 }
 
 char Lexer::peekCharacter() {
-    return readCharacter(currentReaderPosition + 1);
+    return readCharacter(currentReaderPosition);
 }
 
 char Lexer::readCharacter(unsigned long position) {
-    if (position == content.size() - 1) {
+    if (position > content.size()) {
         endOfFileIsReached = true;
         return 0;
     }
